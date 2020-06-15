3905ea0198a8d960cdf184d52ba643446fc22483
Admin <you@example.com>
2020-06-15 23:53:11 +0700

    up

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CreateCategorySeeder::class);
+        $this->call(CreateUserTableSeeder::class);
     }
 }
